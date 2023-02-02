825d553fd2b992e7337b7ab2e297a7af91350c15
Jordan Shefner <yarden11111@gmail.com>
2023-02-02 10:54:02 +0200

    Revert "add nested routes"
    This reverts commit b1d5adf32d96622381c919e75c5cea73eda3659e.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :games do
-    resources :celebs
-  end
+  resources :celebs
+  resources :games
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
